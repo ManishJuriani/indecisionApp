2810ae24626df7a4d1f86db7f52ee11eb800cebe
Manish Juriani <manish_juriani@persistent.co.in>
2019-08-28 22:20:22 +0530

    LineChart Added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import LineChart from 'react-linechart'
 import IndecisionApp from './components/IndecisionApp'
 
-ReactDOM.render(<IndecisionApp/>,document.getElementById('app'))
+export default class App extends React.Component {
+    render() {
+
+        fetch('http://localhost:5000/ages')
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data)
+        }).catch(console.log)
+
+        const data = [
+            {									
+                color: "steelblue", 
+                points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}] 
+            }
+        ];
+        return (
+            <div>
+                <div className="App">
+                    <h1>My First LineChart</h1>
+                    <LineChart 
+                        width={600}
+                        height={400}
+                        data={data}
+                    />
+                </div>				
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'))
